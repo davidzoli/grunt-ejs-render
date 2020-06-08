8d41fa72403c46657afd1128dbaa7187b5b24e9e
Dávid Zoltán <zoli.david@gmail.com>
2020-06-08 10:39:13 +0200

    Update render.js
    - Making it to use only the renderPartial funcionality. 
    - Use YAML file for settings
    - Files can be used in renderPartial() with or without _ prefix and .ejs extension. (_somefile.ejs == somefile.ejs == somefile)

--- tasks/render.js
+++ tasks/render.js
@@ -11,10 +11,9 @@
 module.exports = function(grunt) {
 
 	var ejs = require('ejs'),
-		path = require('path'),
-		fs = require('fs'),
-		marked = require('marked'),
-		_ = require('lodash');
+		 path = require('path'),
+		   fs = require('fs'),
+		    _ = require('lodash');
 
 	//add `underscore.string` for deprecated `grunt.util._` compat
 	_.str = require('underscore.string');
@@ -22,7 +21,6 @@ module.exports = function(grunt) {
 
 	function render(filepath, options) {
 		var src = '';
-
 		if (grunt.file.exists(filepath)) {
 			src = grunt.file.read(filepath);
 			return ejs.render(src, options || null);
@@ -32,14 +30,19 @@ module.exports = function(grunt) {
 		}
 	}
 
-
-	function getFile (filepath, paths) {
+	function getFile(filepath, paths) {
 		var fpath,
 			exists = false;
 
 		exists = (paths || []).some(function (p) {
-			fpath = path.join(p, filepath);
-			return grunt.file.isFile(fpath);
+		  fpath = path.join(p, filepath);
+      if (grunt.file.isFile(fpath)) return true;
+
+      fpath = path.join(p, path.dirname(filepath) + '/_' + path.basename(filepath));
+      if (grunt.file.isFile(fpath)) return true;
+
+      fpath = path.join(p, path.dirname(filepath) + '/_' + path.basename(filepath) + '.ejs');
+      if (grunt.file.isFile(fpath)) return true;
 		});
 
 		if (exists) {
@@ -50,50 +53,16 @@ module.exports = function(grunt) {
 		}
 	}
 
-	//add markdown parser filter to ejs
-	ejs.filters.md = function (obj) {
-		//force string... then parse
-		//just simple as that
-		return marked(obj.toString());
-	};
-
 	grunt.registerMultiTask('render', 'Renders an EJS template to plain HTML', function() {
 		var options = this.options({
 				helpers: {},
-				//basePath: '', DEPRECATED
-				templates: [],
 				partialPaths: [],
 				"_": _
 			}),
 			datapath,
-			templates = {},
 			methods = {};
 
-		//setup some default methods
-		methods.template = function(tplName, data) {
-			if (!_.has(templates, tplName)) {
-				grunt.log.warn('Unable to find template "' + tplName + '"');
-			} else {
-				return templates[tplName](data);
-			}
-		};
-		methods.getMTime = function(filepath) {
-			var fpath = getFile(filepath, options.partialPaths);
-			if (fpath !== false) {
-				return fs.statSync(fpath).mtime.getTime();
-			}
-			return '';
-		};
-
-		methods.readPartial = function(filepath) {
-			var fpath = getFile(filepath, options.partialPaths);
-			if (fpath !== false) {
-				return grunt.file.read(fpath);
-			}
-			return '';
-		};
-
-			methods.renderPartial = function(filepath, data) {
+    methods.renderPartial = function(filepath, data) {
 			var fpath = getFile(filepath, options.partialPaths);
 			if (fpath !== false) {
 				return render(fpath, _.extend({}, options, {filename: fpath}, data || {}));
@@ -101,10 +70,7 @@ module.exports = function(grunt) {
 			return '';
 		};
 
-		//options.basePath = grunt.template.process(options.basePath);
-
 		if ( _.has(options, 'data')) {
-
 			if ( _.isArray(options.data) ) {
 
 				datapath = [].concat(options.data);
@@ -112,7 +78,7 @@ module.exports = function(grunt) {
 							.map(function(filepath) {
 								return grunt.file.expand({
 									filter: function(src) {
-										return grunt.file.isFile(src) && (path.extname(src) === '.json');
+										return grunt.file.isFile(src) && (path.extname(src) === '.yaml');
 									}
 								}, grunt.config.process(filepath));
 							})
@@ -122,40 +88,23 @@ module.exports = function(grunt) {
 
 				options.data = {};
 				datapath.forEach(function (file) {
-					var filename = path.basename(file, '.json');
+					var filename = path.basename(file, '.yaml');
 					var keyName = _.camelize( _.slugify(filename) );
-					options.data[keyName] = grunt.file.readJSON(file);
+					options.data[keyName] = grunt.file.readYAML(file);
 				});
-
 
 			} else if (_.isFunction(options.data)) {
 				options.data = options.data();
-			} else if (_.isString(options.data)) {
-				//DEPRECATED
-				//Kept for compatibility with older versions < 0.2.2
-				datapath = grunt.template.process(options.data);
-				if (grunt.file.exists(datapath)) {
-					options.data = grunt.file.readJSON(datapath);
-				}
 			}
 		}
 
-		_.each(grunt.file.expand(options.templates), function(tpl) {
-			var tplName = path.basename(tpl, path.extname(tpl)).replace(/[\s\-]/g, '_');
-			templates[tplName] = _.template(grunt.file.read(tpl));
-		});
-
-		//add default methods if not already set
 		options.helpers = _.defaults(options.helpers, methods);
 
-		//make options object accessible to helpers
 		_.forOwn(options.helpers, function(helperFunc, helperName, helpers) {
 			if (_.isFunction(helperFunc)) {
 				helpers[helperName] = _.bind(helperFunc, options);
 			}
 		});
-
-		//options._ = _;
 
 		this.files.forEach(function(file) {
 			var contents = file.src.map(function(filepath) {
@@ -163,9 +112,7 @@ module.exports = function(grunt) {
 				return render(filepath, options);
 			}).join('\n');
 
-			// Write joined contents to destination filepath.
 			grunt.file.write(file.dest, contents);
-			// Print a success message.
 			grunt.log.writeln('Rendered HTML file to "' + file.dest + '"');
 		});
 	});
